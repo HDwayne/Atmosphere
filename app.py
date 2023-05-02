880dc31fbe960eabfe1c150a29904f99b449f72e
HERZBERG Dwayne <hdwayne2002@gmail.com>
2023-05-02 18:56:40 +0200

    update layout

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from helpers.utils import *
-from helpers.TEI48 import main_data as main_data_T48, main_zero as main_zero_T48, main_fonct as main_fonct_T48
-from helpers.TEI49 import main_data as main_data_T49, main_zero as main_zero_T49, main_fonct as main_fonct_T49
+from helpers.TEI48 import main as TEI48
+from helpers.TEI49 import main as TEI49
+from helpers.home import main as HOME
 
 
 def main():
@@ -12,10 +13,10 @@ def main():
     )
 
     with st.sidebar:
-        # html_code = show_Laero_logo(100, [1, 1, 1, 1], margin=[0, 0, 0, 0])
-        # st.markdown(html_code, unsafe_allow_html=True)
+        html_code = show_Laero_logo(100, [1, 1, 1, 1], margin=[0, 0, 0, 0])
+        st.markdown(html_code, unsafe_allow_html=True)
 
-        # print_widgets_separator(sidebar=True)
+        print_widgets_separator(sidebar=True)
 
         choice = st.radio(
             "📊 Sélectionnez la source de données",
@@ -29,17 +30,18 @@ def main():
             site = st.text_input("Saisissez l'adresse du serveur 👇")
     
 
-    st.header('Outils de visualisation des données météorologiques et chimiques')
-
-    st.write("Cet outil permet de visualiser les données provenant d'un analyseur d'ozone et d'un analyseur de monoxyde de carbone (sous forme graphique).")
-    st.write("👈 Commencez par choisir l'origine de données, et puis soit mettez en ligne un fichier local, soit saisissez l'adresse du serveur distant !")
+    if file is None:
+        HOME()
 
     if choice == "📁 Fichier local" and file is not None:
         if not CheckZipFileName(file.name):
             st.sidebar.error('Le nom du fichier ne respecte pas la syntaxe')
+            HOME()
         else:
+            # remove file from session state
             delete_session_state_rule(checkFileName)
 
+            # add file to session state
             added_file = []
             dfs = read_zip_file(file)
             apply_time_dfs(dfs, ['20t_Date'], "%d/%m/%Y,%H:%M:%S")
@@ -50,9 +52,9 @@ def main():
                 else:
                     st.session_state[key] = st.session_state[key]
 
+            # display page
             st.sidebar.success(f'Les fichiers suivants ont été ajoutés : {", ".join(added_file)}')
 
-            st.subheader('Données enregistrées: ')
             with st.expander("Cliquez ici pour consulter les données brutes 👋"):
                 number_file = getNumberFileImpoted()
                 if number_file == 0:
@@ -67,36 +69,17 @@ def main():
             tei48, tei49 = st.tabs(["TEI 48", "TEI 49"])
             
             with tei48:
-                DATA, FONCT, ZERO = st.tabs(["Données principales", "Fonctionnement", "Zéro"])
-                with DATA:
-                    main_data_T48()
-                with FONCT:
-                    main_fonct_T48()
-                with ZERO:
-                    main_zero_T48()
-                b1 = st.button("Telecharger les données moyennées", key="1")
-                b2 = st.button("Telecharger les paramètres de configuration de l\'outil", key="2")
-                if b1:
-                    st.write('Fichier téléchargé.')
-                if b2:
-                    st.write('Fichier téléchargé.')
-                
+                TEI48()
             
             with tei49:
-                DATA, FONCT, ZERO = st.tabs(["Données principales", "Fonctionnement", "Zéro"])
-                with DATA:
-                    main_data_T49()
-                with FONCT:
-                    main_fonct_T49()
-                with ZERO:
-                    main_zero_T49()
-                b1 = st.button("Telecharger les données moyennées", key="3")
-                b2 = st.button("Telecharger les paramètres de configuration de l\'outil", key="4")
-                if b1:
-                    st.write('Fichier téléchargé.')
-                if b2:
-                    st.write('Fichier téléchargé.')
+                TEI49()
 
+            b1 = st.button("Telecharger les données moyennées", key="1")
+            b2 = st.button("Telecharger les paramètres de configuration de l\'outil", key="2")
+            if b1:
+                st.write('Fichier téléchargé.')
+            if b2:
+                st.write('Fichier téléchargé.')
 
 if __name__ == '__main__':
     main()
--- helpers/TEI49.py
+++ helpers/TEI49.py
@@ -2,9 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
-from helpers.filter import filters_widgets, filter_dataframe, getIndexRow, addToInvalidRow
+from helpers.filter import filters_widgets, filter_dataframe
 from helpers.utils import print_widgets_separator
-from streamlit_plotly_events import plotly_events
 
 XAXIS=dict(
     rangeselector=dict(
@@ -33,17 +32,41 @@ XAXIS=dict(
 )
 
 
-YAXYS=dict(
-    side="right",
-)
+def main():
+    DATA, FONCT = st.tabs(["Données principales", "Fonctionnement"])
+    with DATA:
+        main_data()
+    with FONCT:
+        main_fonct()
 
 
 def main_data():
-    st.header('TEI 49 Data')
-
     if 'Pdm_TEI49_Data' in st.session_state:
-        filters_widgets(st.session_state.Pdm_TEI49_Data, "Pdm_TEI49_Data_filter")
         Pdm_TEI49_Data = filter_dataframe(st.session_state.Pdm_TEI49_Data, "Pdm_TEI49_Data_filter")
+
+        with st.container():
+            if 'valid' not in Pdm_TEI49_Data.columns:
+                fig_ozone= px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Ozone')
+            else:
+                fig_ozone = px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Ozone', color='valid', markers=True, color_discrete_sequence=['blue', 'red'])
+            
+            fig_ozone.update_layout(title_text="Mesure d'Ozone")
+            fig_ozone.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_ozone, use_container_width=True)
+
+
+            if 'valid' not in Pdm_TEI49_Data.columns:
+                fig_pression= px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Pression')
+            else:
+                fig_pression = px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Pression', color='valid', markers=True, color_discrete_sequence=['blue', 'red'])
+
+            fig_pression.update_layout(title_text="Mesure de Pression")
+            fig_pression.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_pression, use_container_width=True)
+
+        print_widgets_separator()
+
+        filters_widgets(st.session_state.Pdm_TEI49_Data, "Pdm_TEI49_Data_filter")
         
         print_widgets_separator()
     
@@ -55,106 +78,68 @@ def main_data():
 
         print_widgets_separator()
 
-        # TODO: Color blue if valid, red if not (soould'nt depend on the size of the dataset)
-
-        with st.form(key='form_ozone'):
-            if 'valid' not in Pdm_TEI49_Data.columns:
-                fig_ozone= px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Ozone')
-            else:
-                fig_ozone = px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Ozone', color='valid', markers=True, color_discrete_sequence=['blue', 'red'])
-            
-            fig_ozone.update_layout(title_text="Mesure d'Ozone")
-            fig_ozone.update_layout(xaxis=XAXIS)
-            # fig_ozone = plotly_events(fig_ozone, key="ozone", click_event=False, select_event=True)
-            fig_ozone_event = plotly_events(fig_ozone, key="ozone_event", click_event=False, select_event=True)
-            
-            if st.form_submit_button('definir comme invalides'):
-                rows = getIndexRow(Pdm_TEI49_Data, "Pdm_TEI49_Data_filter", fig_ozone_event, "4d_Ozone")
-                addToInvalidRow("Pdm_TEI49_Data_filter", rows)
-            
-        with st.form(key='form_pression'):
-            if 'valid' not in Pdm_TEI49_Data.columns:
-                fig_pression= px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Pression')
-            else:
-                fig_pression = px.line(data_frame=Pdm_TEI49_Data, x='20t_Date', y='4d_Pression', color='valid', markers=True, color_discrete_sequence=['blue', 'red'])
-
-            fig_pression.update_layout(title_text="Mesure de Pression")
-            fig_pression.update_layout(xaxis=XAXIS)
-            # fig_pression = plotly_events(fig_pression, key="pression")            
-            fig_pression_event = plotly_events(fig_pression, key="pression_event", click_event=False, select_event=True)
-
-            if st.form_submit_button('definir comme invalides'):
-                rows = getIndexRow(Pdm_TEI49_Data, "Pdm_TEI49_Data_filter", fig_pression_event, "4d_Pression")
-                addToInvalidRow("Pdm_TEI49_Data_filter", rows)
-
         if 'Pdm_TEI49_Data_filter' in st.session_state:
             st.subheader('Filtre appliqué (Debug)')
             st.write(st.session_state["Pdm_TEI49_Data_filter"])
     else:
         st.error('Pdm_TEI49_Data n\'est pas dans la session. Merci de charger une archive contenant les données nécessaires.')
-
-
-def main_zero():
-    st.title('TEI 49 Zero')
-    st.info('Cet instrument ne fournit pas de données de zéro.')
-          
+       
 
 def main_fonct():
-    st.header('TEI 49 Fonct')
-
     if 'Pdm_TEI49_Fonct' in st.session_state:
         Pdm_TEI49_Fonct = st.session_state.Pdm_TEI49_Fonct
         st.write(Pdm_TEI49_Fonct.describe().loc[['min', 'max', 'mean']])
 
-        fig_flow = go.Figure()
-        fig_flow.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['4.2f_flowA'], name='4.2f_flowA'))
-        fig_flow.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['4.2f_flowB'], name='4.2f_flowB'))
-        fig_flow.update_layout(title_text="Mesure de débit d'air des cellules A et B")
-        fig_flow.update_layout(xaxis=XAXIS, yaxis=YAXYS)
-        st.plotly_chart(fig_flow, use_container_width=True)
+        with st.container():
+            fig_flow = go.Figure()
+            fig_flow.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['4.2f_flowA'], name='4.2f_flowA'))
+            fig_flow.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['4.2f_flowB'], name='4.2f_flowB'))
+            fig_flow.update_layout(title_text="Mesure de débit d'air des cellules A et B")
+            fig_flow.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_flow, use_container_width=True)
 
-        fig_bkg = go.Figure()
-        fig_bkg.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['3.1f_bkg'], name='3.1f_bkg'))
-        fig_bkg.update_layout(title_text="Mesure de fond")
-        fig_bkg.update_layout(xaxis=XAXIS, yaxis=YAXYS)
-        st.plotly_chart(fig_bkg, use_container_width=True)
+            fig_bkg = go.Figure()
+            fig_bkg.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['3.1f_bkg'], name='3.1f_bkg'))
+            fig_bkg.update_layout(title_text="Mesure de fond")
+            fig_bkg.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_bkg, use_container_width=True)
 
-        fig_coef = go.Figure()
-        fig_coef.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.2f_coef'], name='5.2f_coef'))
-        fig_coef.update_layout(title_text="Mesure de coefficient")
-        fig_coef.update_layout(xaxis=XAXIS, yaxis=YAXYS)
-        st.plotly_chart(fig_coef, use_container_width=True)
+            fig_coef = go.Figure()
+            fig_coef.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.2f_coef'], name='5.2f_coef'))
+            fig_coef.update_layout(title_text="Mesure de coefficient")
+            fig_coef.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_coef, use_container_width=True)
 
-        fig_temp = go.Figure()
-        fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_benchT'], name='5.1f_benchT'))
-        fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_O3lampT'], name='5.1f_O3lampT'))
-        fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_intT'], name='5.1f_intT'))
-        fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_lampSetting'], name='5.1f_lampSetting'))
-        fig_temp.update_layout(title_text="Mesure de température")
-        fig_temp.update_layout(xaxis=XAXIS, yaxis=YAXYS)
-        st.plotly_chart(fig_temp, use_container_width=True)
+            fig_temp = go.Figure()
+            fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_benchT'], name='5.1f_benchT'))
+            fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_O3lampT'], name='5.1f_O3lampT'))
+            fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_intT'], name='5.1f_intT'))
+            fig_temp.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['5.1f_lampSetting'], name='5.1f_lampSetting'))
+            fig_temp.update_layout(title_text="Mesure de température")
+            fig_temp.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_temp, use_container_width=True)
 
-        fig_cell = go.Figure()
-        fig_cell.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['6d_cellAInt'], name='6d_cellAInt'))
-        fig_cell.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['6d_cellBInt'], name='6d_cellBInt'))
-        fig_cell.update_layout(title_text="Mesure d'intensité des cellules A et B")
-        fig_cell.update_layout(xaxis=XAXIS, yaxis=YAXYS)
-        st.plotly_chart(fig_cell, use_container_width=True)
+            fig_cell = go.Figure()
+            fig_cell.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['6d_cellAInt'], name='6d_cellAInt'))
+            fig_cell.add_trace(go.Scatter(x=Pdm_TEI49_Fonct['20t_Date'], y=Pdm_TEI49_Fonct['6d_cellBInt'], name='6d_cellBInt'))
+            fig_cell.update_layout(title_text="Mesure d'intensité des cellules A et B")
+            fig_cell.update_layout(xaxis=XAXIS)
+            st.plotly_chart(fig_cell, use_container_width=True)
     else:
         st.error('Pdm_TEI49_Fonct n\'est pas dans la session. Merci de charger une archive contenant les données nécessaires.')
 
-    # TODO : Finish the data validation (comparind data against parameters) -- point 4 in exigence
-    if 'Pdm_TEI49_Data' and 'Pdm_TEI49_Fonct' in st.session_state:
-        data = st.session_state.Pdm_TEI49_Data
-        fonct = st.session_state.Pdm_TEI49_Fonct
+    # # TODO : Finish the data validation (comparind data against parameters) -- point 4 in exigence
+    # if 'Pdm_TEI49_Data' and 'Pdm_TEI49_Fonct' in st.session_state:
+    #     data = st.session_state.Pdm_TEI49_Data
+    #     fonct = st.session_state.Pdm_TEI49_Fonct
 
-        data = data.set_index('20t_Date')
-        fonct = fonct.set_index('20t_Date')
+    #     data = data.set_index('20t_Date')
+    #     fonct = fonct.set_index('20t_Date')
 
-        lower_threshold = fonct.index[0]
-        data_timestamp = data.index.get_loc(lower_threshold, method='nearest')
-        data_sliced = data.iloc[data_timestamp:]
-        merged_df = pd.merge(data_sliced, fonct, left_index=True, right_index=True)
+    #     lower_threshold = fonct.index[0]
+    #     data_timestamp = data.index.get_loc(lower_threshold, method='nearest')
+    #     data_sliced = data.iloc[data_timestamp:]
+    #     merged_df = pd.merge(data_sliced, fonct, left_index=True, right_index=True)
 
-        st.title("Merged dataframe (DATA & FONCT)")
-        st.dataframe(merged_df)
+    #     st.title("Merged dataframe (DATA & FONCT)")
+    #     st.dataframe(merged_df)
