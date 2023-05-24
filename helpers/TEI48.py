efbdb8a137ae674739d6af83fa881bc9293340a5
billyevan <evandion.kurniadi@gmail.com>
2023-05-24 12:27:34 +0200

    graph bug fixed + invalid datapoints

--- helpers/TEI48.py
+++ helpers/TEI48.py
@@ -1,18 +1,18 @@
 import streamlit as st
-import altair as alt
+import plotly.express as px
+import plotly.graph_objects as go
 
 def data_TEI48():
     if "Pdm_TEI48_Data" in st.session_state["dfs"]:
         TEI48_Data = st.session_state["dfs"]["Pdm_TEI48_Data"]
-        st.write(TEI48_Data)
 
         y_data = st.selectbox(
             "Veuillez choisir les données pour l'axe y.",
             (col for col in TEI48_Data.columns if col != "valid" and col != "20t_Date"),
         )
-        # st.line_chart(TEI48_Data, x="20t_Date", y=y_data)
-        c = alt.Chart(TEI48_Data).mark_line().encode(x ='20t_Date' , y=y_data)
-        st.altair_chart(c, use_container_width=True)
+        fig = px.line(TEI48_Data, x="20t_Date", y=y_data)
+        st.plotly_chart(fig, use_container_width=True)
+
         st.write("Statistiques sur les données brutes")
         st.write(TEI48_Data.describe().loc[["min", "max", "mean", "count"]])
     else:
@@ -36,7 +36,15 @@ def zero_TEI48():
                 and col != "20t_DateMaz"
             ),
         )
-        st.line_chart(TEI48_Zero, x="20t_DateZero", y=y_data)
+
+        if y_data == "5d_moy":
+            invalid_datapoints_max(TEI48_Zero["20t_DateZero"], TEI48_Zero[y_data], 20)
+        elif y_data == "5.1f_ect":
+            invalid_datapoints_max(TEI48_Zero["20t_DateZero"], TEI48_Zero[y_data], 50)
+        else:        
+            fig = px.line(TEI48_Zero, x="20t_DateZero", y=y_data)
+            st.plotly_chart(fig, use_container_width=True)
+
         st.write("Statistiques sur les données brutes")
         st.write(TEI48_Zero.describe().loc[["min", "max", "mean", "count"]])
     else:
@@ -57,12 +65,94 @@ def fonct_TEI48():
                 if col != "valid" and col != "20t_Date"
             ),
         )
-        c = alt.Chart(TEI48_Fonct).mark_line().encode(x ='20t_Date' , y=y_data)
-        st.altair_chart(c, use_container_width=True)
-        # st.line_chart(TEI48_Fonct, x="20t_Date", y=y_data)
+        
+        if y_data == "6.4f_ratio":
+            invalid_datapoints_minmax(TEI48_Fonct["20t_Date"], TEI48_Fonct[y_data], 1.1, 1.2)
+        elif y_data == "6.0f_agci":
+            invalid_datapoints_minmax(TEI48_Fonct["20t_Date"], TEI48_Fonct[y_data], 190000, 210000)
+        elif y_data == "5.2f_flow":
+            invalid_datapoints_min(TEI48_Fonct["20t_Date"], TEI48_Fonct[y_data], 1)
+        elif y_data == "6.1f_biasvoltage":
+            invalid_datapoints_minmax(TEI48_Fonct["20t_Date"], TEI48_Fonct[y_data], -110, -120)
+        else:        
+            fig = px.line(TEI48_Fonct, x="20t_Date", y=y_data)
+            st.plotly_chart(fig, use_container_width=True)        
+        
         st.write("Statistiques sur les données brutes")
         st.write(TEI48_Fonct.describe().loc[["min", "max", "mean", "count"]])
     else:
         st.error(
             "Pdm_TEI49_Fonct n'est pas dans la session. Merci de charger une archive contenant les données nécessaires."
         )
+
+        """
+        fig, ax = plt.subplots()
+        ax.plot(TEI48_Fonct["20t_Date"], TEI48_Fonct[y_data])
+        ax.set_xlabel("20t_Date")
+        ax.set_ylabel(str(y_data))
+        
+        st.pyplot(fig)
+        """
+def invalid_datapoints_minmax(x, y, min, max):
+    # Define the interval and the color for points outside the interval
+    interval_min = min
+    interval_max = max
+    outside_color = 'red'
+
+    # Create a list to store the colors of each data point
+    colors = []
+
+    # Iterate over the data points and assign colors based on the interval
+    for xi, yi in zip(x, y):
+        if yi < interval_min or yi > interval_max:
+            colors.append(outside_color)
+        else:
+            colors.append('blue')  # Default color for points inside the interval
+
+    # Create the line chart
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers', marker=dict(color=colors)))
+
+    # Display the chart
+    st.plotly_chart(fig, use_container_width=True)
+
+def invalid_datapoints_min(x, y, min):
+    # Define the interval and the color for points outside the interval
+    interval_min = min
+    outside_color = 'red'
+
+    # Create a list to store the colors of each data point
+    colors = []
+
+    # Iterate over the data points and assign colors based on the interval
+    for xi, yi in zip(x, y):
+        if yi < interval_min:
+            colors.append(outside_color)
+        else:
+            colors.append('blue')  # Default color for points inside the interval
+
+    # Create the line chart
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers', marker=dict(color=colors)))
+
+    # Display the chart
+    st.plotly_chart(fig, use_container_width=True)
+
+def invalid_datapoints_max(x, y, max):
+    # Define the interval and the color for points outside the interval
+    interval_max = max
+    outside_color = 'red'
+
+    # Create a list to store the colors of each data point
+    colors = []
+
+    # Iterate over the data points and assign colors based on the interval
+    for xi, yi in zip(x, y):
+        if yi > interval_max:
+            colors.append(outside_color)
+        else:
+            colors.append('blue')  # Default color for points inside the interval
+
+    # Create the line chart
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers', marker=dict(color=colors)))
+
+    # Display the chart
+    st.plotly_chart(fig, use_container_width=True)
--- helpers/TEI49.py
+++ helpers/TEI49.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import plotly.express as px
+import plotly.graph_objects as go
 
 def data_TEI49():
     if "Pdm_TEI49_Data" in st.session_state["dfs"]:
@@ -9,7 +10,10 @@ def data_TEI49():
             "Veuillez choisir les données pour l'axe x.",
             (col for col in TEI49_Data.columns if col != "valid" and col != "20t_Date"),
         )
-        st.line_chart(TEI49_Data, x="20t_Date", y=y_data)
+
+        fig = px.line(TEI49_Data, x="20t_Date", y=y_data)
+        st.plotly_chart(fig, use_container_width=True)
+
         st.write("Statistiques sur les données brutes")
         st.write(TEI49_Data.describe().loc[["min", "max", "mean", "count"]])
     else:
@@ -29,10 +33,64 @@ def fonct_TEI49():
                 if col != "valid" and col != "20t_Date"
             ),
         )
-        st.line_chart(TEI49_Fonct, x="20t_Date", y=y_data)
+
+        if y_data == "4.2f_flowA":
+            invalid_datapoints_min(TEI49_Fonct["20t_Date"], TEI49_Fonct[y_data], 1)
+        elif y_data == "4.2f_flowB":
+            invalid_datapoints_min(TEI49_Fonct["20t_Date"], TEI49_Fonct[y_data], 1)
+        elif y_data == "6d_cellAInt":
+            invalid_datapoints_min(TEI49_Fonct["20t_Date"], TEI49_Fonct[y_data], 80000)
+        elif y_data == "6d_cellBInt":
+            invalid_datapoints_min(TEI49_Fonct["20t_Date"], TEI49_Fonct[y_data], 80000)
+        else:
+            fig = px.line(TEI49_Fonct, x="20t_Date", y=y_data)
+            st.plotly_chart(fig, use_container_width=True)
+
         st.write("Statistiques sur les données brutes")
         st.write(TEI49_Fonct.describe().loc[["min", "max", "mean", "count"]])
     else:
         st.error(
             "Pdm_TEI49_Data n'est pas dans la session. Merci de charger une archive contenant les données nécessaires."
         )
+
+def invalid_datapoints_min(x, y, min):
+    # Define the interval and the color for points outside the interval
+    interval_min = min
+    outside_color = 'red'
+
+    # Create a list to store the colors of each data point
+    colors = []
+
+    # Iterate over the data points and assign colors based on the interval
+    for xi, yi in zip(x, y):
+        if yi < interval_min:
+            colors.append(outside_color)
+        else:
+            colors.append('blue')  # Default color for points inside the interval
+
+    # Create the line chart
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers', marker=dict(color=colors)))
+
+    # Display the chart
+    st.plotly_chart(fig, use_container_width=True)
+
+def invalid_datapoints_max(x, y, max):
+    # Define the interval and the color for points outside the interval
+    interval_max = max
+    outside_color = 'red'
+
+    # Create a list to store the colors of each data point
+    colors = []
+
+    # Iterate over the data points and assign colors based on the interval
+    for xi, yi in zip(x, y):
+        if yi > interval_max:
+            colors.append(outside_color)
+        else:
+            colors.append('blue')  # Default color for points inside the interval
+
+    # Create the line chart
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers', marker=dict(color=colors)))
+
+    # Display the chart
+    st.plotly_chart(fig, use_container_width=True)
